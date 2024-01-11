001d1feb4846ba958453447d10d7028dce2adfe1
gabriel <dgabrielfof@gmail.com>
2024-01-11 16:25:00 -0300

    :sparkles: feat: adding the event type argument to the input creator

--- src/scripts/components/input.js
+++ src/scripts/components/input.js
@@ -1,19 +1,27 @@
-export function entry({state, key, text, type, validationFunction}) {
+import { cleanOrCreateBox } from "./elementTools.js";
+
+export function entry({state, key, text, type, eventType, validationFunction}) {
   const input = document.createElement("input");
-  input.textContent = text;
+  input.id = `${key}-input`;
+  input.placeholder = text;
   input.type = type;
-  input.addEventListener("input", (e) => {
+  input.addEventListener(eventType, (e) => {
     state.values[key] = e.target.value;
-    const errors = validationFunction();
-    if (errors.length > 0) {
-      const div = document.createElement("div");
-      div.classList.add("error-box");
-      input.insertAdjacentElement("afterend", div);
-      errors.forEach(erro => {
-        const span = createElement("span");
-        span.textContent = erro;       
+    state.values[`${key}Errors`] = [];
+    const keepGoing = validationFunction();
+    const div = cleanOrCreateBox("error-box");
+    if (!keepGoing) {
+      input.classList.add("error")
+      state.values[`${key}Errors`].forEach(erro => {
+        const span = document.createElement("span");
+        span.textContent = erro;
+        div.appendChild(span)     
       });
+      const reference = document.querySelector(`#${key}-input`)
+      reference.insertAdjacentElement("afterend", div);
+    } else {
+      input.classList.remove("error")
     }
-  })
+  });
   return input;
 }
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,38 @@
 import { entry } from "./components/input.js";
 import { removeToast, toast } from "./components/toast.js";
 import { cleanOrCreateBox } from "./components/elementTools.js"
+import { nameValidation } from "./validations/name.js";
+import { emailValidation } from "./validations/email.js";
 
 export const state = {
-
+  values: {
+    name: null,
+    nameErrors: [],
+    email: null,
+    emailErrors: [],
+  },
+  settings: {
+    name: {
+      minSize: 2,
+      maxSize: 18,
+      allowNumbers: false,
+      allowLetters: true,
+      allowSpecialCharacteres: false,
+      needNumbers: false,
+      needLetters: true,
+      needSpecialCharacteres: false,
+    },
+    password: {
+      minSize: 6,
+      maxSize: 12,
+      allowNumbers: true,
+      allowLetters: true,
+      allowSpecialCharacteres: true,
+      needNumbers: true,
+      needLetters: true,
+      needSpecialCharacteres: true,
+    },
+  },
 }
 
 function init () {
@@ -12,12 +41,38 @@ function init () {
   const centralContainer = cleanOrCreateBox("central-container");
   container.appendChild(centralContainer); 
 
-  const inputName = entry("name", "Nome", "text", console.log("texto"));
-  centralContainer.appendChild(inputName);
+  centralContainer.appendChild(
+    entry({
+      state: state,
+      key: "name",
+      text: "seu nome",
+      type: "text",
+      eventType: "input",
+      validationFunction: () => nameValidation(state.values.name, state.settings.name, state.values.nameErrors),
+    })
+  );
 
-  const inputEmail = entry("email", "E-mail", "email", console.log("texto"));
-  centralContainer.appendChild(inputEmail);
-  
+  centralContainer.appendChild(entry({
+    state: state,
+    key: "email",
+    text: "seu e-mail",
+    type: "email",
+    eventType: "input",
+    validationFunction: () => emailValidation(state.values.email, state.values.emailErrors),
+  }));
+
+  const buttonArea = cleanOrCreateBox("buttons-area");
+
+  const confirmButton = document.createElement("button");
+  confirmButton.textContent = "confirmar";
+  buttonArea.appendChild(confirmButton);
+
+  const cancelButton = document.createElement("button");
+  cancelButton.textContent = "cancelar";
+  buttonArea.appendChild(cancelButton);
+
+  centralContainer.appendChild(buttonArea);
+
   const fade = cleanOrCreateBox("fade");
   container.appendChild(fade);
 
