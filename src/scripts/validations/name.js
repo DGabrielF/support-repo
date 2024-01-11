08cef40e00df22a6c9c251915ec209685f5f1c20
gabriel <dgabrielfof@gmail.com>
2024-01-11 16:13:19 -0300

    :bug: fix: import modules in name validation

--- src/scripts/validations/name.js
+++ src/scripts/validations/name.js
@@ -1,45 +1,48 @@
-export function nameValidation(name, nameSettings, arrayOfErrors) {
+import { emptyEntry, entryGreaterThanMaxSize, entryLowerThanMinSize } from "./size.js";
+import { letterExistenceValidation, numberExistenceValidation, specialCharExistenceValidation } from "./regex.js";
+
+export function nameValidation(name, nameSettings, nameErrors) {
   let errorFree = true;
   if (emptyEntry(name)) {
-    arrayOfErrors.push("O nome não pode estar vazio");
+    nameErrors.push("O nome não pode estar vazio");
     errorFree = false;
-  }
+  } 
   if (entryLowerThanMinSize(name, nameSettings.minSize)) {
-    arrayOfErrors.push(`O nome deve conter ao menos ${nameSettings.minSize} letra${nameSettings.minSize > 1 ? "s" : ""}`);
+    nameErrors.push(`O nome deve conter ao menos ${nameSettings.minSize} letra${nameSettings.minSize > 1 ? "s" : ""}`);
     errorFree = false;
   }
   if (entryGreaterThanMaxSize(name, nameSettings.maxSize)) {
-    arrayOfErrors.push(`O nome deve conter ao menos ${nameSettings.maxSize} letras`);
+    nameErrors.push(`O nome deve conter no máximo ${nameSettings.maxSize} letras`);
     errorFree = false;
   }
   if (nameSettings.allowLetters === false) {
     if (!letterExistenceValidation(name, false)) {
-      arrayOfErrors.push(`O nome não deve conter letras`);
+      nameErrors.push(`O nome não deve conter letras`);
       errorFree = false;
     }
-  }
+  } 
   if (nameSettings.allowNumbers === false) {
     if (!numberExistenceValidation(name, false)) {
-      arrayOfErrors.push(`O nome não deve conter números`);
+      nameErrors.push(`O nome não deve conter números`);
       errorFree = false;
     }
   }
   if (nameSettings.allowSpecialCharacteres === false) {
     if (!specialCharExistenceValidation(name, false)) {
-      arrayOfErrors.push(`O nome não deve conter caracteres especiais`);
+      nameErrors.push(`O nome não deve conter caracteres especiais`);
       errorFree = false;
     }
   }
   if (!letterExistenceValidation(name, nameSettings.needLetters)) {
-    arrayOfErrors.push(`O nome ${(nameSettings.needLetters ? "deve" : "não deve")} conter letras`);
+    nameErrors.push(`O nome ${(nameSettings.needLetters ? "deve" : "não deve")} conter letras`);
     errorFree = false;
   }
   if (!numberExistenceValidation(name, nameSettings.needNumbers)) {
-    arrayOfErrors.push(`O nome ${(nameSettings.needLetters ? "deve" : "não deve")} conter números`);
+    nameErrors.push(`O nome ${(nameSettings.needLetters ? "deve" : "não deve")} conter números`);
     errorFree = false;
   }
   if (!specialCharExistenceValidation(name, nameSettings.needSpecialCharacteres)) {
-    arrayOfErrors.push(`O nome ${(nameSettings.needLetters ? "deve" : "não deve")} conter caracteres especiais`);
+    nameErrors.push(`O nome ${(nameSettings.needLetters ? "deve" : "não deve")} conter caracteres especiais`);
     errorFree = false;
   }
   return errorFree;
